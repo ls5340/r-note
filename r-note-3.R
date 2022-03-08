c426be70f676c77ab1191d7bf391fe733b3ca5ff
ls5340 <ls929@cornell.edu>
2022-03-08 12:27:34 -0500

    update

--- r-note-3.R
+++ r-note-3.R
@@ -1,46 +1,45 @@
 library(tidyverse)
+install.packages("readxl")
+library(readxl)
+install.packages("googlesheets4")
+library(googlesheets4)
+install.packages("janitor")
+library(janitor)
 
-table1
+lotr <- read_csv("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/lotr_tidy.csv")
 
-table1 %>% 
-  mutate(rate = cases / population)
+write_csv(lotr, file = "data/lotr.csv")
 
-table1 %>% 
-  summarize(year, counts = sum(cases))
+lotr_disk <- read_csv("./data/lotr.csv", skip = 1)
+lotr_disk <- read_csv("./data/lotr.csv", comment = "#")
+lotr_disk <- read_csv("./data/lotr.csv", comment = "#", na = c("NA"), col_types = cols(Words = col_integer()))
+lotr_disk <- read_csv("./data/lotr.csv", col_names= FALSE)
 
-table1 %>% 
-  summarize(year, wt = cases)
+lotr_disk
+problems(lotr_disk)
 
-table4a %>% 
-  pivot_longer(c(`1999`,`2000`), names_to = "year", values_to = "cases")
+lotr_disk %>% 
+  mutate(Words = parse_integer(Words))
 
-table2 %>% 
-  pivot_wider(names_from = type, values_from = count)
+#lotr_xl <- read_xlsx("data/LOTR.xlsx, sheet = "FOTR")
 
-table3 %>% 
-  separate(rate, into = c("cases","population"), convert = TRUE)
+gs4_deauth()
 
-table3 %>% 
-  separate(year, into = c("century", "year"), sep = 2) %>% 
-  unite(fullyear, century, year, sep = "")
+#lotr_gs <- read_sheet("https://docs.google.com/spreadsheets/d/1X98JobRtA3JGBFacs_JSjiX-4DPQ0vZYtNl_ozqF6IE/edit#gid=754443596")
 
+parse_double("1.23")
+parse_double("1,23", locale = locale(decimal_mark = ","))
+parse_number("123.456.224", locale = locale(grouping_mark = "."))
+parse_number("$100")
+parse_number("20%")
+parse_number("It cost $123.45")
+parse_number("$123,456,789")
+parse_number("123.456.789", locale = locale(grouping_mark = "."))
+parse_number("123'456'789", locale = locale(grouping_mark = "'"))
+parse_number("It costs $20")
 
-# LOTR --------------------------------------------------------------------
+mess = read_tsv("https://raw.githubusercontent.com/nt246/NTRES6940-data-science/master/datasets/messy_data.tsv", locale = locale(decimal_mark = ","))
 
+mess %>% 
+  mutate(price = parse_number(price))
 
-fship <- read_csv("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/The_Fellowship_Of_The_Ring.csv")
-fship
-
-ttow <- read_csv("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/The_Two_Towers.csv")
-ttow
-
-rking <- read_csv("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/The_Return_Of_The_King.csv")
-rking
-
-lotr_untidy <- bind_rows(fship, ttow, rking)
-
-lotr_tidy <- lotr_untidy %>% 
-  pivot_longer(c(Female, Male), names_to = "Gender", values_to = "Words")
-
-lotr_wide <- lotr_tidy %>% 
-  pivot_wider(names_from = Race, values_from = Words)
---
+++ r-note-4.R
@@ -0,0 +1,23 @@
+#vignettes()
+#dput()
+#https://cscu.cornell.edu/consulting/drop-ins/
+
+#lotr <- read_csv("https://raw.githubusercontent.com/jennybc/lotr-tidy/master/data/lotr_tidy.csv")
+#lotr_space <-  read_delim("", delim = "\t")
+#lotr_space
+
+#write_csv(lotr, file = " .csv")
+#write_tsv(lotr, file = " .tsv")
+
+library(tidyverse)
+library(janitor)
+
+coronavirus <- read_csv('https://raw.githubusercontent.com/RamiKrispin/coronavirus/master/csv/coronavirus.csv')
+plot <- coronavirus %>% 
+  filter(type == "confirmed") %>% 
+  group_by(date) %>% 
+  summarize(total_cases = sum(cases)) %>% 
+  ggplot() +
+  geom_line(aes(date,total_cases))
+ggsave(plot, filename = "~/Home/NTRES6100 r note/data/totalcounuts.jpg") 
+#ggsave(plot, filename = "~/Home/NTRES6100 r note/data/totalcounuts.png") 
